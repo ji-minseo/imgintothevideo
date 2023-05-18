abbc9d29bc86273ec64482ca60bd2939ce9da30d
ji-minseo <jijiminseo@gmail.com>
2023-05-18 15:08:10 +0900

    add img into the vid

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let video, cl, ctxl, c_tmp, ctx_tmp;
+let video, img, cl, ctxl, c_tmp, ctx_tmp;
 function init() {
   video = document.getElementById('video')
 
   cl = document.getElementById('output-canvas')
   ctxl = cl.getContext('2d')
+
+  img = new Image();
+
+  img.src = "/asset/img.png"
 
   c_tmp = document.createElement('canvas')
   c_tmp.setAttribute('width', 800)
@@ -16,13 +20,18 @@ function init() {
 function computeFrame() {
   ctx_tmp.drawImage(video, 0, 0, video.videoWidth/10, video.videoHeight/5)
   let frame = ctx_tmp.getImageData(0, 0, video.videoWidth/10, video.videoHeight/5)
-  
+
+  ctx_tmp.drawImage(img, 0, 0, video.videoWidth/10, video.videoHeight/5)
+  let frame2 = ctx_tmp.getImageData(0, 0, video.videoWidth/10, video.videoHeight/5)
+
   for (let i = 0; i < frame.data.length / 4; i++) {
     let r = frame.data[i * 4 + 0]
     let g = frame.data[i * 4 + 1]
     let b = frame.data[i * 4 + 2]
     if (r > 40 && r < 90 && g > 100 && g < 200 && b > 10 && b < 170) {
-      frame.data[i * 4 + 3] = 0
+      frame.data[i * 4 + 0] = frame2.data[i * 4 + 0]
+      frame.data[i * 4 + 1] = frame2.data[i * 4 + 1]
+      frame.data[i * 4 + 2] = frame2.data[i * 4 + 2]
     }
   
   }
