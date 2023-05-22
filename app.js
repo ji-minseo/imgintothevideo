c19d062c2d2e964e7f521a751018f30f736eb144
ji-minseo <jijiminseo@gmail.com>
2023-05-22 12:10:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let video, img, cl, ctxl, c_tmp, ctx_tmp, input, imagesArray, imgWidth, imgHeight, frame3, frame2, frame4, ctx_tmp2;
+let video, img, cl, ctxl, c_tmp, ctx_tmp, input, imagesArray, imgWidth, imgHeight;
 function init() {
   video = document.getElementById('video')
 
@@ -19,7 +19,6 @@ function init() {
   c_tmp.setAttribute('width', 800)
   c_tmp.setAttribute('height', 1200)
   ctx_tmp = c_tmp.getContext('2d')
-  ctx_tmp2 = c_tmp.getContext('2d')
 
   video.addEventListener('play', computeFrame)
 
@@ -35,11 +34,10 @@ function init() {
 function computeFrame() {
   ctx_tmp.drawImage(video, 0, 0, video.videoWidth/10, video.videoHeight/5)
   let frame = ctx_tmp.getImageData(0, 0, video.videoWidth/10, video.videoHeight/5)
-  frame3 = ctx_tmp.createImageData(video.videoWidth/10, video.videoHeight/5);
 
 
-  ctx_tmp.drawImage(img, 0, 0, imgWidth/5, imgHeight/3)
-  frame2 = ctx_tmp.getImageData(0, 0, imgWidth, imgHeight)
+  ctx_tmp.drawImage(img, 0, 0, imgWidth/5, imgHeight/1.5)
+  let frame2 = ctx_tmp.getImageData(0, 0, imgWidth/5, imgHeight/1.5)
   let startX = 0;
   let endX = 0
   for (let i = 0; i < frame.data.length / 4; i++) {
@@ -49,37 +47,22 @@ function computeFrame() {
     if (r > 40 && r < 90 && g > 100 && g < 200 && b > 10 && b < 170) {
       if(startX == 0) {
         startX = i
+        console.log(i)
       }
-      translateImg(startX)
-      setTimeout(() => {
-        frame4 = ctx_tmp2.getImageData(0, 0, video.videoWidth/10, video.videoHeight/5)
-
-        frame.data[i * 4 + 0] = frame4.data[i * 4 + 0]
-        frame.data[i * 4 + 1] = frame4.data[i * 4 + 1]
-        frame.data[i * 4 + 2] = frame4.data[i * 4 + 2]
-  
-      },30)
+      // if(endX < i) endX = i
+      // console.log(endX)
+    
+      frame.data[i * 4 + 0] = frame2.data[(i - startX) * 4 + 0]
+      frame.data[i * 4 + 1] = frame2.data[(i - startX) * 4 + 1]
+      frame.data[i * 4 + 2] = frame2.data[(i - startX) * 4 + 2]
     }
+  
   }
+
+
+
   ctxl.putImageData(frame, 0, 0)
   setTimeout(computeFrame, 0)
-}
-
-function translateImg(startX) {
-  for (let y = 0; y < frame2.height; y++) {
-    for (let x = 0; x < frame2.width; x++) {
-      // 현재 픽셀의 인덱스 계산
-      const index = (y * frame2.width + x) * 4;
-  
-      // 이동된 좌표의 픽셀 값을 가져와서 임시 배열에 저장합니다.
-      frame3.data[startX * 4] = frame2.data[index];
-      frame3.data[startX * 4 + 1] = frame2.data[index + 1];
-      frame3.data[startX * 4 + 2] = frame2.data[index + 2];
-      frame3.data[startX * 4 + 3] = frame2.data[index + 3];
-
-      ctx_tmp2.putImageData(frame3, 0, 0);
-    }
-  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
